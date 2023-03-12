e01d418244ce6adf9262afbb0bf6b422d290e06f
HilarioNengareJr <38634516+HilarioNengareJr@users.noreply.github.com>
2023-03-12 16:54:09 +0300

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -7,18 +7,25 @@ class Solution:
         l = 0
         
         # testing the base condition
-        while h - l >= 1:
+        while h >= l:
             m = (h+l) // 2
             # if mid element is less than target check right subarray
             if (self.nums[m] < self.target):
                 l = m+1
+            # if mid element is greater than target element check the left subarray
             elif (self.nums[m] > self.target):
                 h = m-1
+            # else the target element is equal to the mid element then return that index
             else:
                 return m
         return -1
-
-obj = Solution()
-arr = [12]
-target = 12
-print(obj.search(arr,target))
+    
+def main():
+    obj = Solution()
+    arr = [11,12]
+    target = 12
+    # print the return index of the target element
+    print(obj.search(arr,target))
+    
+if __name__ == "__main__":
+    main()
