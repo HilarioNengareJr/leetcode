b2501f264d57dbd72667071370ac280a1d5b4dd4
HILARIO NENGARE <38634516+HilarioNengareJr@users.noreply.github.com>
2023-08-29 17:58:44 +0300

    Using two pointer algorithm to solve the twosum.

--- twosum_with_two_pointer.py
+++ twosum_with_two_pointer.py
@@ -1,23 +1,20 @@
-class Solution(object):
+class Solution:
     def twoSum(self, nums, target):
-        self.nums = nums
-        self.target = target
-
         i = 0
-        j = len(self.nums) - 1
+        j = len(nums) - 1
         result = []
 
-        while i < j:
-            if self.nums[i] + self.nums[j] == self.target:
-                result.append(i)
-                result.append(j)
-                return (result)
-            elif self.nums[i] + self.nums[j] > self.target:
-                j -= 1
-            else:
+        while nums[i] + nums[j] != target:
+            if nums[i] + nums[j] < target:
                 i += 1
-        return None
-    
-s = Solution()
-nums = [2,7,11,15]
-print(s.twoSum(nums, 9))
+            else:
+                j -= 1
+
+        result.append(i)
+        result.append(j)
+        return result
+
+
+nums = [3, 2, 4]
+solution = Solution()
+print(solution.twoSum(nums, 6))
