3d1a6cd1e034104b1415b1362e2db35d68b46ccf
HILARIO NENGARE <38634516+HilarioNengareJr@users.noreply.github.com>
2023-08-30 17:28:15 +0300

    Updated twosum_with_two_pointer.py

--- twosum_with_two_pointer.py
+++ twosum_with_two_pointer.py
@@ -1,20 +1,18 @@
 class Solution:
     def twoSum(self, nums, target):
-        i = 0
-        j = len(nums) - 1
-        result = []
+        nums_sorted = [(num, index) for index, num in enumerate(nums)]
+        nums_sorted.sort()
+        print(nums_sorted)
+        i , j = 0, len(nums_sorted)-1
 
-        while nums[i] + nums[j] != target:
-            if nums[i] + nums[j] < target:
+        while nums_sorted[i][0] + nums_sorted[j][0] != target:
+            if nums_sorted[i][0] + nums_sorted[j][0] < target:
                 i += 1
             else:
                 j -= 1
 
-        result.append(i)
-        result.append(j)
-        return result
+        return [nums_sorted[i][1], nums_sorted[j][1]]
 
-
-nums = [3, 2, 4]
+nums = [-1,-2,-3,-4,-5]
 solution = Solution()
-print(solution.twoSum(nums, 6))
+print(solution.twoSum(nums, -8))
