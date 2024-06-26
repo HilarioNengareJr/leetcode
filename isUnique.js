f1f08bd14b97bd090dab02df34a4d504f18762bf
HILARIO NENGARE <38634516+HilarioNengareJr@users.noreply.github.com>
2024-06-26 15:23:41 +0300

    Updated isUnique.js

--- isUnique.js
+++ isUnique.js
@@ -23,3 +23,29 @@ function isUnique(someString){
 }
 
 console.log(isUnique('hilario'));
+
+
+// Using set data structure
+
+/**
+ *  using a set which holds unique elements
+ **/
+ 
+function isUnique(str){
+    let charSet = new Set();
+    
+    for(let elm of str){
+        // if a set contains the element we are trying to add then we return false
+        if(charSet.has(elm)){
+            return false;
+        }
+        
+        charSet.add(elm);
+    }
+    
+    // means string is unqiue
+    return true;
+}
+
+console.log(isUnique('Hilaro')); // true
+console.log(isUnique('Hilario')); // false
