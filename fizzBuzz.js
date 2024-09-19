f96b9168a2c8a5158fa717ac3b0b798cf25217d0
HILARIO NENGARE <38634516+HilarioNengareJr@users.noreply.github.com>
2024-09-19 05:13:10 +0300

    Create fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,18 @@
+function FizzBuzz(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return result.join(" ");
+}
+
+// Example usage:
+console.log(FizzBuzz(8));
